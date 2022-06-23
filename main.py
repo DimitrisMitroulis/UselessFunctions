942a87980a3b12ddfa6fc33f1fb591505ffc7f27
DimitrisMitroulis <amfimitrul@gmail.com>
2022-06-23 23:58:04 +0300

    added add function

--- main.py
+++ main.py
@@ -7,12 +7,50 @@ import random
 from random import randint
 
 
+# supports up to 5+5
+# calculates and returns the sum of 2 numbers
+def add(num1, num2):
+    if num1 == 0 and num2 == 0: return 0
+    if num1 == 1 and num2 == 0: return 1
+    if num1 == 1 and num2 == 1: return 2
+    if num1 == 0 and num2 == 1: return 1
+    if num1 == 2 and num2 == 0: return 2
+    if num1 == 2 and num2 == 1: return 3
+    if num1 == 2 and num2 == 2: return 2
+    if num1 == 1 and num2 == 2: return 3
+    if num1 == 0 and num2 == 2: return 2
+    if num1 == 3 and num2 == 0: return 3
+    if num1 == 3 and num2 == 1: return 4
+    if num1 == 3 and num2 == 2: return 5
+    if num1 == 2 and num2 == 3: return 5
+    if num1 == 1 and num2 == 3: return 4
+    if num1 == 2 and num2 == 3: return 5
+    if num1 == 4 and num2 == 0: return 4
+    if num1 == 4 and num2 == 1: return 5
+    if num1 == 4 and num2 == 2: return 6
+    if num1 == 4 and num2 == 3: return 7
+    if num1 == 4 and num2 == 4: return 8
+    if num1 == 1 and num2 == 4: return 5
+    if num1 == 2 and num2 == 4: return 6
+    if num1 == 3 and num2 == 4: return 7
+    if num1 == 5 and num2 == 0: return 5
+    if num1 == 5 and num2 == 1: return 6
+    if num1 == 5 and num2 == 2: return 7
+    if num1 == 5 and num2 == 3: return 8
+    if num1 == 5 and num2 == 4: return 9
+    if num1 == 5 and num2 == 5: return 10
+    if num1 == 1 and num2 == 5: return 6
+    if num1 == 2 and num2 == 5: return 7
+    if num1 == 3 and num2 == 5: return 8
+    if num1 == 4 and num2 == 5: return 9
+    # if num1 == and num2 ==: return
+
+
 # returns requested number
 def return_number(number):
-    randnum = random.randint(number-10, number+10)
+    randnum = random.randint(number - 10, number + 10)
     while randnum != number:
-        randnum = random.randint(number-10, number+10)
-
+        randnum = random.randint(number - 10, number + 10)
 
 
 def coin_flip():
@@ -36,7 +74,7 @@ def coin_flip():
 
 def main():
     print('Main')
-    return_number(15)
+
 
 
 if __name__ == '__main__':
